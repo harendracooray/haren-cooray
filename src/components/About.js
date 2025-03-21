f6a4f0000f6a2fb42f98c650898e91f8af4133fe
Haren <harendra.cooray@gmail.com>
2025-03-21 12:38:02 -0400

    update file commit message

--- src/components/About.js
+++ src/components/About.js
@@ -5,6 +5,7 @@ export default function About() {
   return (
     <div className='about'>
       <div >
+
         <p>Has more than 14 years of experience in software engineering platforms and has delivered many engineered solutions to businesses in multiple domains, including finance, healthcare, and retail. Expertise in User Experience and User Interface Application Development, delivering the best-engineered User Interface solutions using agile methodology and best practices aligned with the business vision, and helping customers meet their business goals.</p>
         <p>Proficiency in developing scalable and interactive web applications using React, Typescript, Node.js, and Next.js. I have successfully implemented responsive user interfaces, ensuring a seamless and engaging experience for end-users. Additionally, my strong background in HTML, CSS, and SCSS Bootstrap, has allowed me to create visually appealing designs while maintaining clean and modular stylesheets.</p>
         <p>What sets me apart is my experience in Adobe AEM component development and ADOBE AEM page authoring. I have actively contributed to projects involving the creation of custom ADOBE AEM components, enabling dynamic content delivery, and enhancing overall website functionality. My expertise in AEM page authoring has empowered content authors to create and manage engaging web content efficiently. Designed and developed websites/pages in Adobe CQ/AEM by implementing the responsive design. Used Adobe CRX, CRXDE, WCM, Package Manager and Components.</p>
